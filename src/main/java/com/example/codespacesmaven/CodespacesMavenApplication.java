5af0d1702beca1f67e5b0a765eddf6720aedbd7f
Eddú Meléndez <eddu.melendez@gmail.com>
2022-07-09 18:24:19 -0500

    Initial commit

---
+++ src/main/java/com/example/codespacesmaven/CodespacesMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.codespacesmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodespacesMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodespacesMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codespacesmaven/CodespacesMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codespacesmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespacesMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
